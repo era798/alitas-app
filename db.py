3415399cca7abf36b38bdc522f7e451902b8a61a
era798 <riosalcantara512@hotmail.com>
2026-08-15 21:14:49 -0600

    Refactor obtener_conexion to improve database setup
    Refactor database connection logic to include setting the search path directly after establishing the connection. Added error handling for missing DATABASE_URL environment variable.

--- db.py
+++ db.py
@@ -7,23 +7,20 @@ def obtener_conexion():
         database_url = os.getenv('DATABASE_URL')
         if database_url:
             conn = psycopg2.connect(database_url)
+            with conn.cursor() as cursor:
+                cursor.execute('SET search_path TO "Alitas", public;')
+            return conn
         else:
-            conn = psycopg2.connect(
-                host="localhost",
-                database="alitas_db",
-                user="postgres",
-                password="Htvatcc6$",
-                port="5432"
-            )
-        return conn
+            print("No se encontró la variable DATABASE_URL")
+            return None
     except Exception as e:
-        print(f"Error al conectar a la base de datos: {e}")
-        return None
+        print(f":Error al conectar a la base de datos: {e}")
+            return None
+        
 if __name__ == "__main__":
     conexion = obtener_conexion()
     if conexion:
         with conexion.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute('SET search_path TO "Alitas", public;')
             cursor.execute("SELECT * FROM productos LIMIT 5;")
             productos = cursor.fetchall()
             print("--- Conexión exitosa ---")
