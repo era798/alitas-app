95091608f75fb659ef454e301e59764773e561ef
era798 <riosalcantara512@hotmail.com>
2026-08-15 22:26:01 -0600

    Set search path for database in obtener_productos
    Added search path setting for database queries.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def obtener_productos():
 
     conexion.set_client_encoding('UTF8')
     cursor = conexion.cursor(cursor_factory=RealDictCursor)
+    cursor.execute('SET search_path TO "Alitas", public;')
     cursor.execute("SELECT * FROM "Alitas".productos ORDER BY categoria ASC, nombre ASC;")
     productos = cursor.fetchall()
     cursor.close()
